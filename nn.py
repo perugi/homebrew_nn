1d15dc1f4e438f8c0a18df6036bae9276fa3c7f7
Dominik Perusko <dominik.perusko@gmail.com>
2022-10-11 10:38:47 +0200

    Non-functional code reformatting.

--- nn.py
+++ nn.py
@@ -4,15 +4,15 @@ import numpy as np
 
 
 def sigmoid(z, der=False):
-    """Returh the sigmoid or sigmoid derivative of value z, based on the der value."""
+    """Return the sigmoid or sigmoid derivative of value z, based on the der value."""
     if der:
-        # return np.exp(-z) / ((1 + np.exp(-z)) ** 2)
         return sigmoid(z) * (1 - sigmoid(z))
     else:
         return 1.0 / (1.0 + np.exp(-z))
 
 
 def softmax(z, der=False):
+    """Return the softmax or softmax derivative of value z, based on the der value."""
     # Shift the z values so the highest is 0 (to avoid calculating very large numbers).
     z -= np.max(z)
 
@@ -37,29 +37,33 @@ class CrossEntropyCost:
 
 
 class Network:
-    def __init__(self, layers, cost_fn):
+    def __init__(self, layers, cost_fn=QuadraticCost):
         """Initialize the neural network, based on the layers array, passed to it.
         The length of the array represents the number of layers (including the input and output layers.
         The elements of the array represent the number of neurons in the individual layers.
         The weights and biases are initialized to random values from the standard normal distribution (stdev=1, mean=0)"""
 
         self.cost_fn = cost_fn
-        self.num_layers = len(layers)
         self.layers = layers
         self.weights = []
         self.biases = []
-        for layer_no in range(1, self.num_layers):
-            self.weights.append(np.random.randn(layers[layer_no], layers[layer_no - 1]))
-            self.biases.append(np.random.randn(layers[layer_no], 1))
+
+        for i in range(1, len(self.layers)):
+            self.weights.append(np.random.randn(layers[i], layers[i - 1]))
+            self.biases.append(np.random.randn(layers[i], 1))
 
     def forward(self, x):
         """Compute the output of the network, based on an input x. The function goes through
         all the layers and uses the weights and biases of each layer to compute the output of a layer
         until the final layer is reached. The sigmoid is used as the activation function."""
 
-        for layer_weights, layer_biases in zip(self.weights, self.biases):
-            x = sigmoid(np.dot(layer_weights, x) + layer_biases)
-        return x
+        # Use the input as the activation of the first layer
+        a = x
+
+        for w, b in zip(self.weights, self.biases):
+            a = sigmoid(np.dot(w, a) + b)
+
+        return a
 
     def SGD(
         self, training_data, epochs, mini_batch_size, learning_rate, test_data=None
@@ -73,21 +77,17 @@ class Network:
         Training data and test data are list of tuples, with the input and label values."""
 
         for epoch in range(epochs):
-            print(f"----- Training epoch {epoch+1}/{epochs} -----")
+            print(f" Training epoch {epoch+1} ".center(30, "-"))
 
-            # Shuffle and create mini batches.
+            # Shuffle and create mini batches
             random.shuffle(training_data)
-            mini_batches = [
-                training_data[i : i + mini_batch_size]
-                for i in range(0, len(training_data), mini_batch_size)
-            ]
-
-            for mini_batch in mini_batches:
+            for i in range(0, len(training_data), mini_batch_size):
+                mini_batch = training_data[i : i + mini_batch_size]
                 self.update_mini_batch(mini_batch, learning_rate)
 
             if test_data:
                 print(
-                    f"Correct outputs: {self.evaluate(test_data)} out of {len(test_data)}"
+                    f"Test Accuracy: {self.evaluate(test_data) / len(test_data) * 100}%"
                 )
 
     def update_mini_batch(self, mini_batch, learning_rate):
@@ -130,21 +130,16 @@ class Network:
 
         # Move forward through the network, storing the weighted inputs (z) activations (a) of each neuron.
         weighted_inputs = []
-        activation = x
-        activations = [activation]
+        activations = [x]
 
         for layer_weights, layer_biases in zip(self.weights, self.biases):
-            weighted_input = np.dot(layer_weights, activation) + layer_biases
+            weighted_input = np.dot(layer_weights, activations[-1]) + layer_biases
             activation = sigmoid(weighted_input)
 
             weighted_inputs.append(weighted_input)
             activations.append(activation)
 
         # Calculate the output error, giving us also the errors of weights and biases for the output layer.
-        # output_error = np.multiply(
-        #     self.cost_derivative(activations[-1], y),
-        #     sigmoid(weighted_inputs[-1], der=True),
-        # )
         output_error = self.cost_fn.error(activations[-1], y, weighted_inputs[-1])
 
         errors_weights[-1] = np.dot(output_error, activations[-2].transpose())
@@ -152,7 +147,7 @@ class Network:
 
         # Backpropagate the output error, calculating the errors of weights and biases for each layer.
         layer_error = output_error
-        for layer_no in range(2, self.num_layers):
+        for layer_no in range(2, len(self.layers)):
             layer_error = np.multiply(
                 np.dot(self.weights[-layer_no + 1].transpose(), layer_error),
                 sigmoid(weighted_inputs[-layer_no], der=True),
@@ -170,8 +165,8 @@ class Network:
         a list of tuples, containing the inputs with matchind labels."""
 
         count_correct = 0
-        for (input, label) in test_data:
-            if np.argmax(self.forward(input)) == label:
+        for (x, y) in test_data:
+            if np.argmax(self.forward(x)) == y:
                 count_correct += 1
 
         return count_correct
--- testing.py
+++ testing.py
@@ -20,10 +20,18 @@ training_data, test_data = mnist_loader.load_data()
 #     net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
 # print(training_data[0][0].shape)
 
+print("Training with Quadratic CF")
+for i in range(1, 4):
+    print("")
+    print(f" NN no. {i} ".center(30, "#"))
+    print("")
+    net = nn.Network([784, 30, 10], nn.QuadraticCost)
+    net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
+print("Training with Cross Entropy CF")
 for i in range(1, 4):
     print("")
     print(f"##### NN no. {i} #####")
     print("")
     net = nn.Network([784, 30, 10], nn.CrossEntropyCost)
     net.SGD(training_data, 30, 10, 0.5, test_data=test_data)
-print(training_data[0][0].shape)
